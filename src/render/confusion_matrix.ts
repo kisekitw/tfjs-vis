1f465b86dc99b795a6d488a552ce502d2d64e96a
Yannick Assogba <yassogba@google.com>
2018-12-20 10:51:50 -0500

    save

--- src/render/confusion_matrix.ts
+++ src/render/confusion_matrix.ts
@@ -46,9 +46,9 @@ import {getDrawArea} from './render_utils';
  * @param container An `HTMLElement` or `Surface` in which to draw the chart
  * @param opts optional parameters
  * @param opts.shadeDiagonal boolean that controls whether or not to color cells
- * on the diagonal. Defaults to true.
+ * on the diagonal. Defaults to true
  * @param opts.showTextOverlay boolean that controls whether or not to render
- * the values of each cell as text. Defaults to true.
+ * the values of each cell as text. Defaults to true
  * @param opts.width width of chart in px
  * @param opts.height height of chart in px
  * @param opts.fontSize fontSize in pixels for text in the chart
